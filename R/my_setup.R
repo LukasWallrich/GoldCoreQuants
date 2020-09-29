9a50a4b0a124158fd65ea62471d3ec3db3fe99bd
Lukas Wallrich <lukas.wallrich@gmail.com>
2020-09-29 18:59:25 +0100

    set CRAN

--- R/my_setup.R
+++ R/my_setup.R
@@ -8,4 +8,9 @@ video_code <- function(code) {
   video(paste0("https://www.youtube.com/embed/", code, "?rel=0&modestbranding=1&loop=1&playlist=", code))
 }
 
+local({r <- getOption("repos")
+r["CRAN"] <- "https://cloud.r-project.org" 
+options(repos=r)
+})
+
 if (!require("pacman")) install.packages("pacman")
